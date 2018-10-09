1ff77c14cbe8f5530cfd7360a176702481c23cfd
fomich486 <42343682+fomich486@users.noreply.github.com>
2018-10-09 14:18:26 +0300

    Ofline tic-tac-toe
    Game without multiplayer.
    If in future i want to expand field(Make it 4x4 or 5x5) it  will lead to an error because all checkings made for 3x3 version.

---
+++ Tic-Tac-Toe/Assets/Scripts/Controller.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+	const int size = 3;
+	[SerializeField]
+	List<Text> txt;
+	[SerializeField]
+	Image gameOver;
+	[SerializeField]
+	Text gameOverText;
+	void Start () {
+		GameStart ();
+	}
+
+	public void CheckForWin(string side)
+	{
+		//draw
+		if (NoButtonInteractable ()) {
+			GameOver("DRAW!");
+		}
+		//row Check
+		if (txt [0].text == side && txt [1].text == side && txt [2].text == side) {
+			GameOver (side);
+			
+		}
+		if (txt [3].text == side && txt [4].text == side && txt [5].text == side) {
+			GameOver (side);
+			
+		}
+		if (txt [6].text == side && txt [7].text == side && txt [8].text == side) {
+			GameOver (side);
+			
+		}
+		//colum
+		if (txt [0].text == side && txt [3].text == side && txt [6].text == side) {
+			GameOver (side);
+			
+		}
+		if (txt [1].text == side && txt [4].text == side && txt [7].text == side) {
+			GameOver (side);
+			
+		}
+		if (txt [2].text == side && txt [5].text == side && txt [8].text == side) {
+			GameOver (side);
+			
+		}
+		//Diagonals
+		if (txt [0].text == side && txt [4].text == side && txt [8].text == side) {
+			GameOver (side);
+			
+		}
+		if (txt [6].text == side && txt [4].text == side && txt [2].text == side) {
+			GameOver (side);
+		}
+	}
+
+	void GameOver(string side)
+	{
+		if(side != "DRAW!")
+			side = side + " Wins!";
+		foreach (Text t in txt) {
+			t.GetComponentInParent<Button>().interactable = false;
+		}
+		gameOver.gameObject.SetActive (true);
+		gameOverText.text = side;
+	}
+	void GameStart()
+	{
+		gameOver.gameObject.SetActive (false);
+		foreach (Text t in txt) {
+			t.GetComponentInParent<Button> ().interactable = true;
+			t.text = " ";
+		}
+	}
+
+	public void Restart()
+	{
+		GameStart ();
+	}
+
+	bool NoButtonInteractable()
+	{
+		bool check = false;
+		for (int i = 0; i < txt.Count; i++) {
+			if (txt [i].GetComponentInParent<Button> ().interactable) {
+				check = false;
+				break;
+			}
+			check = true;
+		}
+		return check;
+	}
+}
